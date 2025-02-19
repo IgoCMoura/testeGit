be3f956f30952bbcbf71447037dc9c19c5121366
IgoCMoura <igocmoura@gmail.com>
2025-02-19 17:47:04 -0300

    Primeira versãoi

---
+++ Animal.ts
@@ -0,0 +1,29 @@
+﻿export class Animal{
+    private nome: string;
+    private idade: number;
+
+    constructor(n: string, i: number){
+        this.nome = n;
+        this.idade = i;
+    }
+
+    mostrarNome(): string{
+        return this.nome
+    }
+
+    mostrarIdade(): number{
+        return this.idade;
+    }
+    emitirSom(): void{
+        console.log("Eu vou emitir meu this.setNome, se prepare")
+    }
+
+    setNome(novoNome: string): void{
+        this.nome =novoNome;
+    }
+
+    setIdade(novaIDade: number): void{
+        this.idade = novaIDade;
+    }
+
+}
